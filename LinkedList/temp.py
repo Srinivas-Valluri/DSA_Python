624a070f39f8220facc15d17ce7cdb9e57095217
Srinivas-Valluri <vallurisrinivas09@gmail.com>
2023-10-20 09:23:07 +0530

    leetcode + LinkedList

---
+++ LinkedList/temp.py
@@ -0,0 +1,14 @@
+class Node:
+  def __init__(self, value) -> None:
+    self.value = value
+    self.next = None
+
+class LinkedList:
+  def __init__(self, value) -> None:
+    new_node = Node(value)
+    self.head = new_node
+    self.tail = new_node
+
+ll = LinkedList(10)
+
+print(ll)
--- List/temp2.py
+++ List/temp2.py
@@ -1,6 +1,5 @@
-a = ["sri"]
+nestedList = [1,[4,[6]]]
+a = 1
 
-print(a[0:1])
-
-print(a[0])
-print(type(a[0]))
+if type(nestedList[1]) == "int":
+  print("hello")
